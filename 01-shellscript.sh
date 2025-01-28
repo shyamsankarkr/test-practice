5a1fef7d87476423d79b467dd77e88ce72434e1e
shyamsankarkr <krjayasankar146@gmail.com>
2025-01-28 13:29:08 +0530

    made changes

--- 01-shellscript.sh
+++ 01-shellscript.sh
@@ -1,2 +1,18 @@
-#!/bin/bash
-echo "hello world"
+
+USERId=$(id -u)
+
+if[ USERId -ne 0 ]
+then
+   echo "run it with sudo access"
+   exit 1
+fi
+ 
+yum install mysql -y
+
+if[ $? -ne 0 ]
+then
+  echo "error in installation"
+  exit 1
+else 
+  echo "mysql successfully installed"
+fi
