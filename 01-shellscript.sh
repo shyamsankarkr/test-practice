018f11f9c3d0f10fb39f3c1736a846b727c563e3
shyamsankarkr <krjayasankar146@gmail.com>
2025-01-28 16:22:40 +0530

    made changes

--- 01-shellscript.sh
+++ 01-shellscript.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
 DATE=$(date +%F)
-SCRIPTNAME=$0
-LOGFILE=/tmp/$SCRIPTNAME-$DATE.log
+SCRIPTNAME=$(basename $0)
+LOGFILE="/tmp/$SCRIPTNAME-$DATE.log"
 USERId=$(id -u)
 R="\e[31m"
 
-VALIDATE() {
+VALIDATE(){
   if [ $1 -ne 0 ]; then
-    echo "error in installation"
+    echo -e "${R}Error in installation"
     exit 1
-  else 
+  else
     echo "$2 successfully installed"
   fi
 }
 
 if [ $USERId -ne 0 ]; then
-  echo "run it with sudo access"
+  echo "Run it with sudo access"
   exit 1
 fi
 
-# Install ansible and suppress logs in terminal
-apt install ansible -y &>/dev/null
+# Suppress all apt output including progress
+DEBIAN_FRONTEND=noninteractive apt install ansible -y -qq &>>$LOGFILE
 
 VALIDATE $? "ansible"
